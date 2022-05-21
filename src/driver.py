509811bef3d26e42c8daa0883ddcbaa585cdc1f2
azuki774 <azuki774s@gmail.com>
2022-05-21 14:21:06 +0900

    add option no gpu

--- src/driver.py
+++ src/driver.py
@@ -5,11 +5,13 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 
+
 def get_driver():
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument("--disable-dev-shm-usage") 
-    chrome_service = Service(executable_path='/usr/bin/chromedriver')
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-dev-shm-usage")
+    chrome_service = Service(executable_path="/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=chrome_service, options=options)
     return driver
