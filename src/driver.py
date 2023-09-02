684382066b2c0061bd6a3169176b8e3d73c44391
azuki774s <azuki774s@gmail.com>
2023-09-02 18:33:18 +0900

    use package fix and revise default driver

--- src/driver.py
+++ src/driver.py
@@ -1,4 +1,3 @@
-from venv import create
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
@@ -7,11 +6,14 @@ from bs4 import BeautifulSoup
 
 
 def get_driver():
-    options = Options()
+    options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
+    options.add_argument("--lang=ja-JP")
     options.add_argument("--disable-dev-shm-usage")
+    UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
+    options.add_argument("--user-agent=" + UA)
     chrome_service = Service(executable_path="/usr/bin/chromedriver")
     driver = webdriver.Chrome(service=chrome_service, options=options)
     return driver
